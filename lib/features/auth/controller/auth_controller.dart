0ce4aef3c567507b0a9cb0ab5fd39fc01894f90c
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-08-27 23:19:06 +0545

    complete auth flow

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/core/utils.dart';
 import 'package:flutter_reddit_clone/features/auth/repository/auth_repository.dart';
+import 'package:flutter_reddit_clone/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final authControllerProvider = Provider(
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
     authRepository: ref.read(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
+  final Ref _ref;
 
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false);
 
-  void signInWithGoogle(BuildContext context) {
+  void signInWithGoogle(BuildContext context) async {
     try {
-      _authRepository.signInWithGoogle();
+      state = true;
+      final userModel = await _authRepository.signInWithGoogle();
+      state = false;
+      _ref.read(userProvider.notifier).update((state) => userModel);
       // do something
     } catch (e) {
       showSnackBar(context, e.toString());
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_reddit_clone/core/common/firebase_constants.dart';
 import 'package:flutter_reddit_clone/core/contants/constants.dart';
-import 'package:flutter_reddit_clone/core/failure.dart';
 import 'package:flutter_reddit_clone/core/providers/firebase_providers.dart';
 import 'package:flutter_reddit_clone/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -32,7 +31,7 @@ class AuthRepository {
   CollectionReference get _users =>
       _firestore.collection(FirebaseConstants.usersCollection);
 
-  Future<UserModel> signInWithGoogle() async {
+  Future<UserModel?> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication? googleAuth =
@@ -45,7 +44,7 @@ class AuthRepository {
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
 
-      late UserModel userModel;
+      UserModel userModel;
       if (userCredential.additionalUserInfo!.isNewUser) {
         userModel = UserModel(
           uid: userCredential.user!.uid,
@@ -58,11 +57,19 @@ class AuthRepository {
           awards: [],
         );
         await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      } else {
+        userModel = await getUserData(userCredential.user!.uid).first;
       }
-
       return userModel;
     } catch (e) {
       rethrow;
     }
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _users
+        .doc(uid)
+        .snapshots()
+        .map((user) => UserModel.fromMap(user.data() as Map<String, dynamic>));
   }
 }
